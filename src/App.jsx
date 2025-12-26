34ebe7e62049e6dc0be6770d5b3af594e45eaf53
Cathy Yu <z5590011@ad.unsw.edu.au>
2025-12-26 17:58:12 +0800

    added links to footer

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,11 @@ function App() {
 
       <nav className="m-0 h-[50px] w-full fixed left-0 right-0 bg-red-200 flex flex-row px-10 bottom-0 justify-evenly text-center">
         <div className="flex items-center justify-center justify-evenly text-center">
-          <img src="/github-mark-white.svg" className="h-[30px] w-[30px] mr-5"></img>
+          <img src="/github-mark-white.svg" className="h-[30px] w-[30px] mr-5 hover:scale-120 transition-transform duration-200"
+            onClick={() => window.open("https://github.com/kittymaru")}></img>
 
-          <img src="/linkedin-svgrepo-com.svg" className="h-[30px] w-[30px]"></img>
+          <img src="/linkedin-svgrepo-com.svg" className="h-[30px] w-[30px] hover:scale-120 transition-transform duration-200"
+            onClick={() => window.open("https://www.linkedin.com/in/cathy-yu-938b6b261/")}></img>
         </div>
       </nav>
     </>
