66bff26f7fb1d0db4b7a2082fc09d73be4a27c0b
Cathy Yu <z5590011@ad.unsw.edu.au>
2025-12-26 11:39:35 +0800

    added header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,15 @@ import './App.css'
 
 function App() {
   return (
-    <div className="text-red-500">placeholder</div>
+    <>
+      <nav className="m-0 h-[80px] w-full fixed left-0 right-0 bg-red-300 flex flex-row px-10 top-0 justify-evenly text-center">
+        navbar
+      </nav>
+
+      <nav className="m-0 h-[80px] w-full fixed left-0 right-0 bg-red-300 flex flex-row px-10 bottom-0 justify-evenly text-center">
+        footer
+      </nav>
+    </>
   )
 }
 
